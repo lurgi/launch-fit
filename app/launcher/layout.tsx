901e595c3578a9c3ef07bf5b2391241b7b4b4572
Jeongwoo Park <wahoo9040@naver.com>
2025-01-20 08:39:52 +0900

    chore: 주석 삭제

--- app/launcher/layout.tsx
+++ app/launcher/layout.tsx
@@ -2,11 +2,8 @@
 import LauncherSidebar from "@/components/app/launcher/LauncherSidebar";
 
 export default function LauncherLayout({ children }: { children: React.ReactNode }) {
-  //TODO: 로그인 확인 로직 추가
-
   return (
     <div className="flex min-h-screen">
-      {/*TODO: 로그아웃 로직 추가 */}
       <LauncherSidebar />
 
       <main className="w-full px-6 md:px-10 max-h-screen overflow-y-auto flex flex-col items-center">
