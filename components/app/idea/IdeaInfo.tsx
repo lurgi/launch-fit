2fc189a132d5b11e7fa0b53cfef80367df73e46d
Jeongwoo Park <wahoo9040@naver.com>
2025-01-20 21:24:34 +0900

    fix[Early Bird]: IdeaInfo 스켈레톤 수정

--- components/app/idea/IdeaInfo.tsx
+++ components/app/idea/IdeaInfo.tsx
@@ -1,26 +1,12 @@
-"use client";
 import { cn } from "@/lib/utils";
-import { useEffect, useState } from "react";
 
 export default function IdeaInfo({ title, description }: { title?: string; description?: string }) {
-  const [isShowSkeleton, setIsShowSkeleton] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsShowSkeleton(true);
-    }, 1000);
-  }, []);
-
   return (
     <section className="w-full max-w-2xl text-center">
-      <h1
-        className={cn("text-3xl font-bold sm:text-4xl h-10", !title && isShowSkeleton && "bg-gray-200 animate-pulse")}
-      >
+      <h1 className={cn("text-3xl font-bold sm:text-4xl ", !title && "bg-gray-200 animate-pulse h-10")}>
         {title && title}
       </h1>
-      <p
-        className={cn("mt-4 text-lg text-gray-600 h-7", !description && isShowSkeleton && "bg-gray-200 animate-pulse")}
-      >
+      <p className={cn("mt-4 text-lg text-gray-600 ", !description && "bg-gray-200 animate-pulse h-7")}>
         {description && description}
       </p>
     </section>
