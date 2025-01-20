c050aeb3806605cd45f95f6c505fab494486ef13
Jeongwoo Park <wahoo9040@naver.com>
2025-01-20 17:47:12 +0900

    feat[Launcher]: 반응형 패딩 수정

--- app/launcher/layout.tsx
+++ app/launcher/layout.tsx
@@ -6,7 +6,7 @@ export default function LauncherLayout({ children }: { children: React.ReactNode
     <div className="flex min-h-screen">
       <LauncherSidebar />
 
-      <main className="w-full px-6 md:px-10 max-h-screen overflow-y-auto flex flex-col items-center">
+      <main className="w-full px-2 sm:px-6 md:px-10 lg:px-16 max-h-screen overflow-y-auto flex flex-col items-center">
         <div className="px-6 flex flex-col items-center max-w-3xl w-full">{children}</div>
       </main>
     </div>
