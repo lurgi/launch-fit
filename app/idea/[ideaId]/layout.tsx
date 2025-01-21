cbbab0680d99e4ee74c7e37cb6b7f1f3c70dd1c5
Jeongwoo Park <wahoo9040@naver.com>
2025-01-21 14:31:25 +0900

    feat[SEO]: meta 태그 설정
    squash
    
    squash

---
+++ app/idea/[ideaId]/layout.tsx
@@ -0,0 +1,22 @@
+const getFetcher = async (url: string) => {
+  const response = await fetch(url);
+  return response.json();
+};
+
+export async function generateMetadata({ params }: { params: Promise<{ ideaId: string }> }) {
+  const { ideaId } = await params;
+  const idea = await getFetcher(`${process.env.NEXT_PUBLIC_DOMAIN_URL}/api/publicIdea?ideaId=${ideaId}`);
+
+  return {
+    title: idea.title,
+    description: idea.description,
+    openGraph: {
+      title: idea.title,
+      description: idea.description,
+    },
+  };
+}
+
+export default function IdeaLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
