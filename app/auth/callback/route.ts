2782ef77929b0af1a817fbffe70e05d68ab03719
Jeongwoo Park <wahoo9040@naver.com>
2025-01-21 15:32:27 +0900

    fix: 카카오 리디렉트 로직 수정

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -4,7 +4,6 @@ import { createServerClient } from "@/lib/supabase/serverClient";
 export async function GET(request: NextRequest) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
-  // if "next" is in param, use it as the redirect URL
   const next = searchParams.get("next") ?? "/launcher/idea-dashboard";
   const headers = new Headers();
 
@@ -13,19 +12,14 @@ export async function GET(request: NextRequest) {
     const { error } = await supabase.auth.exchangeCodeForSession(code);
 
     if (!error) {
-      const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
       const isLocalEnv = process.env.NODE_ENV === "development";
       if (isLocalEnv) {
-        // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
         return NextResponse.redirect(`${origin}${next}`, { headers });
-      } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`, { headers });
       } else {
-        return NextResponse.redirect(`${origin}${next}`, { headers });
+        return NextResponse.redirect(`${process.env.NEXT_PUBLIC_DOMAIN_URL}${next}`, { headers });
       }
     }
   }
 
-  // return the user to an error page with instructions
   return NextResponse.redirect(`${origin}/auth/signin`);
 }
