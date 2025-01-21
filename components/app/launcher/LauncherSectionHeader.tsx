9da3e016c5f4a47646f829d08cf3165922c5aa8a
Jeongwoo Park <wahoo9040@naver.com>
2025-01-21 09:01:22 +0900

    fix: 페이지 상세 헤더 수정

--- components/app/launcher/LauncherSectionHeader.tsx
+++ components/app/launcher/LauncherSectionHeader.tsx
@@ -1,19 +1,17 @@
-"use client";
-
 import { cn } from "@/lib/utils";
-import { useEffect, useState } from "react";
 
 export default function LauncherSectionHeader({ title, description }: { title?: string; description?: string }) {
-  const [isShowSkeleton, setIsShowSkeleton] = useState(true);
-
-  useEffect(() => {
-    setIsShowSkeleton(false);
-  }, []);
-
   return (
     <section className="w-full max-w-2xl text-center py-12">
-      <h1 className={cn("text-3xl font-bold sm:text-4xl h-10", isShowSkeleton && "animate-pulse")}>{title}</h1>
-      <p className={cn("mt-4 text-lg text-gray-600 h-6", isShowSkeleton && "animate-pulse")}>{description}</p>
+      <h1 className={cn("text-3xl font-bold sm:text-4xl min-h-10", title || "bg-gray-200 animate-pulse")}>{title}</h1>
+      <p
+        className={cn(
+          "mt-4 text-lg text-gray-600 min-h-6 max-h-32 overflow-y-hidden line-clamp-4",
+          description || "bg-gray-200 animate-pulse"
+        )}
+      >
+        {description}
+      </p>
     </section>
   );
 }
