e1e00725ed1294ddce24af2e317fcc391d00fa67
Jeongwoo Park <wahoo9040@naver.com>
2025-01-21 14:29:36 +0900

    fix[Auth]: Layout 수정

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -3,8 +3,10 @@ import React from "react";
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center px-6">
-      {children}
+    <div className="min-w-screen min-h-screen flex flex-col items-center justify-center px-6 pb-32 relative">
+      <main className="flex-1 flex w-full flex-col items-center justify-center relative overflow-y-auto my-12">
+        {children}
+      </main>
       <HomeFooter />
     </div>
   );
