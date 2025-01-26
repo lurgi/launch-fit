654ad8cc6944d02ae190e3e648dab8c51b3fa4a6
Jeongwoo Park <wahoo9040@naver.com>
2025-01-26 12:40:52 +0900

    fix: LinkToIdeaButton 스타일 수정
    squash

--- components/app/launcher/LinkToIdeaButton.tsx
+++ components/app/launcher/LinkToIdeaButton.tsx
@@ -7,11 +7,13 @@ interface LinkToIdeaButtonProps {
 
 export default function LinkToIdeaButton({ ideaId }: LinkToIdeaButtonProps) {
   return (
-    <Link href={`/idea/${ideaId}`} target="_blank">
-      <div className="flex items-center justify-center gap-2 h-9 bg-amber-500 hover:bg-amber-600 text-white rounded-lg">
-        <ArrowRightIcon className="w-6 h-6" />
-        바로 가기
-      </div>
+    <Link
+      href={`/idea/${ideaId}`}
+      target="_blank"
+      className="flex items-center justify-center gap-2 h-9 py-2 px-6 bg-amber-500 hover:bg-amber-600 text-sm text-white rounded-lg"
+    >
+      <ArrowRightIcon className="w-6 h-6 p-[1px]" />
+      바로 가기
     </Link>
   );
 }
